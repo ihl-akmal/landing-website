02670683b2847ad15494dea005c79845b17a67bb
ihl-akmal <info.akmalindonesia@gmail.com>
2025-07-22 16:07:00 +0800

    update logo navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,11 +25,13 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-2">
-            <img
-              src="/logo-grazedu-website.svg"
-              alt="Grazedu Logo"
-              className="h-8 w-auto"
-            />
+            <a href="/" className="flex items-center">
+              <img
+                src="/logo-grazedu-website.svg"
+                alt="Grazedu Logo"
+                className="h-8 w-auto"
+              />
+            </a>
             {/* <Heart className="h-5 w-5 text-primary" /> */}
           </div>
 
