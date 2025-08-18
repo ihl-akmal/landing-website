3f34920f218a89a77b305733d696c547cbfed5c7
ihl-akmal <info.akmalindonesia@gmail.com>
2025-08-18 12:24:35 +0800

    perbaikan gtag dan layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${poppins.variable} font-sans`}>
         <Providers>{children}</Providers>
