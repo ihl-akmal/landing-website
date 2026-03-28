8d2cb6522624810dbc04e02a32db6a4d039f3fd7
ihl-akmal <info.akmalindonesia@gmail.com>
2026-03-28 14:23:05 +0800

    fix: perubahan tampilan halaman not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-white text-center px-6">
-      <h1 className="text-4xl font-bold text-gray-800 mb-3">Kelas Tidak Ditemukan</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-3">Halaman Tidak Ditemukan</h1>
       <p className="text-gray-600 mb-8 max-w-md">
-        Sepertinya kelas yang kamu cari sudah tidak aktif atau telah ditutup.  
-        Yuk, jelajahi kelas lain yang masih tersedia!
+        Sepertinya halaman yang kamu cari tidak tersedia.
+
       </p>
       <Link
         href="/"
